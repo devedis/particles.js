ee139fdfce6394d09bf9cef36313b135a7c99b30
SimpleDay <pascal.giehl@gmail.com>
2018-02-04 13:14:12 +0100

    Fixed Internet Explorer Bug
    element.remove() is not supported in IE

--- particles.js
+++ particles.js
@@ -1235,7 +1235,7 @@ var pJS = function(tag_id, params){
 
   pJS.fn.vendors.destroypJS = function(){
     cancelAnimationFrame(pJS.fn.drawAnimFrame);
-    canvas_el.remove();
+    canvas_el.parentNode.removeChild(canvas_el);
     pJSDom = null;
   };
 
